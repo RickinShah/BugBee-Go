19f13c1dcb646ef5db051968fec21d39d25f3e8f
Rickin Shah <rickin.shah17403@gmail.com>
2025-01-04 22:20:00 +0530

    change router to httprouter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,24 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
-	router := http.NewServeMux()
+	router := httprouter.New()
 
-	router.HandleFunc("GET  /v1/healthcheck", app.healthcheckHandler)
-	router.HandleFunc("GET  /v1/posts/{id}", app.showPostHandler)
-	router.HandleFunc("POST  /v1/posts", app.createPostHandler)
-	router.HandleFunc("PATCH  /v1/posts/{id}", app.updatePostHandler)
-	router.HandleFunc("DELETE  /v1/posts/{id}", app.deletePostHandler)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.HandleFunc("POST /v1/otp", app.generateOtpHandler)
-	router.HandleFunc("POST /v1/otp/validate", app.validateOtpHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
-	router.HandleFunc("POST /v1/users", app.registerUserHandler)
-	router.HandleFunc("PUT /v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/posts/:id", app.showPostHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/posts/:id", app.createPostHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/posts/:id", app.updatePostHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/posts/:id", app.deletePostHandler)
 
-	router.HandleFunc("POST /v1/tokens/authentication", app.createAuthenticationHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/otp", app.generateOtpHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/otp/validate", app.validateOtpHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationHandler)
 
 	//return app.recoverPanic(app.rateLimit(router))
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.authenticate(router))
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/boombuler/barcode v1.0.2 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/mail.v2 v2.3.1 // indirect
